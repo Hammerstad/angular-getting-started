640dffd8508c816cb7b7b96f2ad38bf504cb9eaf
Eirik Mildestveit Hammerstad <emhammerstad@gmail.com>
2019-06-17 08:28:50 +0200

    Routing moved to separate modules

---
+++ src/app/routing/product-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from '../products/product-detail.guard';
+import { ProductDetailComponent } from '../products/product-detail.component';
+import { ProductListComponent } from '../products/product-list.component';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      { path: "products", component: ProductListComponent },
+      {
+        path: "products/:id",
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent
+      }
+    ]),
+  ],
+  exports: [RouterModule]
+})
+export class ProductRoutingModule { }
